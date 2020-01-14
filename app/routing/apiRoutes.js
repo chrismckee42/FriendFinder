94a115489e2e5e3a6bb28bb2eaafbbb45575d92e
Christopher McKee <chris.mckee.13@gmail.com>
2020-01-13 18:14:39 -0800

    adds link to friends.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,6 @@
-const friends = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-}]
+var friendFile = require("../data/friends.js")
+var friends = friendFile.friends
+
 
 module.exports = function (app) {
     app.get("/api/friends", function (req, res) {
